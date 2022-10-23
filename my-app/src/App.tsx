32f1525bede9af282efa9c214ca50dc1f7623d29
brian teka sala <briantekasala@hotmail.be>
2022-10-23 22:42:39 +0200

    setup barber web site

---
+++ my-app/src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css';
+import { NavigationMenu } from './navigation/NavigationMenu';
+
+function App() {
+  return (
+    <div className="App">
+      <NavigationMenu></NavigationMenu>
+    </div>
+  );
+}
+
+export default App;
---
+++ my-app/src/navigation/NavigationMenu.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react';
+
+
+export const NavigationMenu = ()=> {
+    return (
+        <div className="navigation">menu</div>
+    )
+}
+
